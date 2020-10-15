6372d1898dbbb2a308018b6c13831b458c204c79
Abhiram13 <abhiram131869@gmail.com>
2020-10-15 15:23:58 +0530

    items from collection will be sent

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
 import { Application } from "express";
-import express = require("express");
-const app:Application = express();
+import e = require("express");
+import { Collection, MongoClient, MongoError } from "mongodb";
+import { Items } from './typedef/types';
 
-app.get('/', function(req: express.Request, res: express.Response) {
-     res.send('Hello World!');
+const mongo: MongoClient = require("mongodb");
+const app: Application = e();
+const uri: string = "mongodb+srv://abhiramDB:abhiram13@myfirstdatabase.l8kvg.mongodb.net/Mordor?retryWrites=true&w=majority";
+
+async function connectToDB(): Promise<Items | null> {
+     const client: MongoClient = new MongoClient(uri, { useUnifiedTopology: true });
+     try {
+          await client.connect();
+          const collections: Collection<Items> = client.db("Mordor").collection<Items>("items");
+          let item: Promise<Items | null> = collections.findOne({ itemName: "Green Milk" });
+
+          return item;
+     } catch (e) {
+          console.log(e);
+          return null;
+     }
+}
+app.get("/", function(req: e.Request, res: e.Response) {
+     res.send("Hello");
 });
 
 app.listen(1996, function() {
+     connectToDB().then((res) => console.log(res));
      console.log('Example app listening on port 1996!');
 });
