0c75908951a903a1176ff417b258b7ca35eefc09
Abhiram13 <abhiram131869@gmail.com>
2020-10-15 16:01:16 +0530

    item list in collections are rendering

--- index.ts
+++ index.ts
@@ -1,18 +1,21 @@
 import { Application } from "express";
 import e = require("express");
-import { Collection, MongoClient, MongoError } from "mongodb";
+import { Collection, Cursor, MongoClient, MongoError } from "mongodb";
 import { Items } from './typedef/types';
 
 const mongo: MongoClient = require("mongodb");
 const app: Application = e();
 const uri: string = "mongodb+srv://abhiramDB:abhiram13@myfirstdatabase.l8kvg.mongodb.net/Mordor?retryWrites=true&w=majority";
 
-async function connectToDB(): Promise<Items | null> {
+async function connectToDB(): Promise<Cursor<Items>| null> {
      const client: MongoClient = new MongoClient(uri, { useUnifiedTopology: true });
      try {
           await client.connect();
           const collections: Collection<Items> = client.db("Mordor").collection<Items>("items");
-          let item: Promise<Items | null> = collections.findOne({ itemName: "Green Milk" });
+          let item: Cursor<Items> = collections.find({});
+          item.forEach(function(item) {
+               console.log(item.itemName);
+          });
 
           return item;
      } catch (e) {
@@ -25,6 +28,6 @@ app.get("/", function(req: e.Request, res: e.Response) {
 });
 
 app.listen(1996, function() {
-     connectToDB().then((res) => console.log(res));
+     connectToDB().then((res) => res);
      console.log('Example app listening on port 1996!');
 });
