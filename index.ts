2a822f5a4380847d73ab8c1c3e5dd225324b63a8
Abhiram13 <abhiram131869@gmail.com>
2020-10-18 09:36:31 +0530

    fetching all documents from db to array

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Application } from "express";
-import e = require("express");
+import e from "express"
 import { Collection, Cursor, MongoClient, MongoError } from "mongodb";
 import { Items } from './typedef/types';
 
@@ -7,19 +7,17 @@ const mongo: MongoClient = require("mongodb");
 const app: Application = e();
 const uri: string = "mongodb+srv://abhiramDB:abhiram13@myfirstdatabase.l8kvg.mongodb.net/Mordor?retryWrites=true&w=majority";
 
-async function connectToDB(): Promise<Cursor<Items>| null> {
+async function connectToDB(): Promise<Items[] | null> {
      const client: MongoClient = new MongoClient(uri, { useUnifiedTopology: true });
+     var array: string[] = [];
      try {
-          await client.connect();
+          await client.connect();          
           const collections: Collection<Items> = client.db("Mordor").collection<Items>("items");
-          let item: Cursor<Items> = collections.find({});
-          item.forEach(function(item) {
-               console.log(item.itemName);
-          });
-
-          return item;
+          let item: Cursor<Items> = collections.find({});          
+          let itemsCollection: Items[] = await item.toArray();
+          return itemsCollection;
      } catch (e) {
-          console.log(e);
+          // console.log(e);
           return null;
      }
 }
@@ -28,6 +26,6 @@ app.get("/", function(req: e.Request, res: e.Response) {
 });
 
 app.listen(1996, function() {
-     connectToDB().then((res) => res);
+     connectToDB().then((res) => console.log(res));
      console.log('Example app listening on port 1996!');
 });
