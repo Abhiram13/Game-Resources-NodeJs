aa9706250490c0629fed443245897243dab04420
Abhiram13 <abhiram131869@gmail.com>
2020-10-18 11:04:03 +0530

    code refactor

--- index.ts
+++ index.ts
@@ -1,30 +1,29 @@
 import { Application } from "express";
 import e from "express";
-import { Collection, Cursor, Db, MongoClient, MongoError } from "mongodb";
+import { Collection, Cursor, Db, MongoClient } from "mongodb";
 import { Items } from './typedef/types';
 
-const mongo: MongoClient = require("mongodb");
 const app: Application = e();
-const uri: string = "mongodb+srv://abhiramDB:abhiram13@myfirstdatabase.l8kvg.mongodb.net/Mordor?retryWrites=true&w=majority";
 
-async function connectToDB(): Promise<Items[] | null> {
-   const client: MongoClient = new MongoClient(uri, { useUnifiedTopology: true });
-   try {
-      await client.connect();
-      const mordor: Db = client.db("Mordor");
-      const collections: Collection<Items> = mordor.collection<Items>("items");
-      let item: Cursor<Items> = collections.find({});
-      let itemsCollection: Items[] = await item.toArray();
-      return itemsCollection;
-   } catch (e) {
-      return null;
+class Mongo {
+   static URI: string = "mongodb+srv://abhiramDB:abhiram13@myfirstdatabase.l8kvg.mongodb.net/Mordor?retryWrites=true&w=majority";
+   static client: MongoClient = new MongoClient(Mongo.URI, { useUnifiedTopology: true });
+   static Database: Db = Mongo.client.db("Mordor");
+   static async Connect(): Promise<void> {
+      try {
+         await Mongo.client.connect();
+      } catch (e: any) {
+         console.log(e.Message);
+      }
    }
 }
+
 app.get("/", function(req: e.Request, res: e.Response) {
    res.send("Hello");
 });
 
 app.listen(1996, function() {
-   connectToDB().then((res) => res);
+   // Connects to MongoDB when starts listening
+   Mongo.Connect();
    console.log('Example app listening on port 1996!');
 });
