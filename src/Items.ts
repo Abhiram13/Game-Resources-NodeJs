86f5711376879cd81e17659205cd4d53d1ab2a7b
Abhiram13 <abhiram131869@gmail.com>
2020-10-19 21:32:21 +0530

    fetching all data from db

--- src/Items.ts
+++ src/Items.ts
@@ -5,10 +5,12 @@ import { Collection, Cursor, Db, MongoClient } from "mongodb";
 import { Items } from '../typedef/types';
 
 export class Item {
-   static FetchAll(request: e.Request, response: e.Response): void {
+   static async FetchAll(request: e.Request, response: e.Response): Promise<void> {
       try {
-         let ItemsCollection: Cursor<Items> = Mongo.client.db("Mordor").collection<Items>("items").find({});
-         response.status(200).send(ItemsCollection.toArray());
+         let ItemsCollection: Cursor<Items> = await Mongo.client.db("Mordor").collection<Items>("items").find({});
+         ItemsCollection.toArray().then(function(items) {
+            response.status(200).send(items);
+         });         
       } catch (e) {
          console.log(e);
          response.status(500);
