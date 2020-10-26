78722bf0f43b09547d3c2d3ca3f62c28d748c33b
Abhiram13 <abhiram131869@gmail.com>
2020-10-26 20:32:04 +0530

    added search item functinality

--- src/Items.ts
+++ src/Items.ts
@@ -1,17 +1,16 @@
-import { Mongo, app } from '../index';
-import { Application } from "express";
+import {Mongo, app} from '../index';
+import {Application} from "express";
 import e from "express";
-import { Collection, Cursor, Db, MongoClient, ObjectID } from "mongodb";
-import { Items } from '../typedef/types';
+import {Collection, Cursor, Db, MongoClient, ObjectID} from "mongodb";
+import {Items} from '../typedef/types';
 
 export class Item {
-   // static collection: Collection<Items> = Mongo.client.db("Mordor").collection<Items>("items");
    static async FetchAll(request: e.Request, response: e.Response): Promise<void> {
       try {
          let ItemsCollection: Cursor<Items> = await Mongo.client.db("Mordor").collection<Items>("items").find({});
          ItemsCollection.toArray().then(function(items) {
             response.status(200).send(items);
-         });         
+         });
       } catch (e) {
          console.log(e);
          response.status(500);
@@ -20,7 +19,7 @@ export class Item {
 
    static async FindById(request: e.Request, response: e.Response): Promise<void> {
       try {
-         await Mongo.client.db("Mordor").collection<Items>("items").findOne({ "_id": new ObjectID(request.params.id) }, function(err, result: Items) {
+         await Mongo.client.db("Mordor").collection<Items>("items").findOne({"_id": new ObjectID(request.params.id)}, function(err, result: Items) {
             if (err) {
                response.status(500).send(err).end();
             } else {
@@ -34,20 +33,22 @@ export class Item {
 
    static async Search(request: e.Request, response: e.Response): Promise<void> {
       try {
-         if (request.body === undefined) {
-            response.status(500).end();
-         }
-
-         console.log(request.body);
-         await Mongo.client.db("Mordor").collection<Items>("items").findOne({ "itemName": request.body.key }, function(err, item) {
-            if (err) {
-               console.log(err);
-               response.status(500).send(err);
-            } else {
-               console.log(item);
-               response.status(200).send(item);
+         await Mongo.client.db("Mordor").collection<Items>("items").find({}).toArray().then(function(items) {
+            let array: Items[] = [];
+            for (var i: number = 0; i < items.length; i++) {
+               if (items[i].itemName.substring(0, request.body.string.length).toUpperCase() === request.body.string.toUpperCase()) {
+                  array.push(items[i]);
+               }
             }
-         })
+
+            console.log(array.length);
+
+            if (array.length > 0) {
+               response.status(200).send(array);
+            } else {
+               response.status(404).end();
+            }
+         });
       } catch (e) {
          //
       }
