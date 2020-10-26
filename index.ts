10fd6fa3f37d7a9ce9a36de2c52ada3c82850f01
Abhiram13 <abhiram131869@gmail.com>
2020-10-26 19:42:11 +0530

    added body parser module

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import { Application, NextFunction } from "express";
 import e from "express";
+import bodyparser from "body-parser";
 import { Collection, Cursor, Db, MongoClient } from "mongodb";
 import { Authorisation, TOKEN } from './helpers/helper';
 import { Item } from './src/Items';
 import { Items } from "./typedef/types";
 
 export const app: Application = e();
-
 export class Mongo {
    static URI: string = "mongodb+srv://abhiramDB:abhiram13@myfirstdatabase.l8kvg.mongodb.net/Mordor?retryWrites=true&w=majority";
    static client: MongoClient = new MongoClient(Mongo.URI, { useUnifiedTopology: true });   
@@ -20,6 +20,8 @@ export class Mongo {
    }
 }
 
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 // All Routes including /item/ will be needed authorisation
 // Once approved, the function will trigger next() method 
@@ -48,6 +50,7 @@ app.get("/second", function(req: e.Request, res: e.Response) {
 
 app.get("/item/findall", (req: e.Request, res: e.Response) => Item.FetchAll(req, res));
 app.get("/item/findone/:id", (req: e.Request, res: e.Response) => Item.FindById(req, res));
+app.post("/item/search", (req: e.Request, res: e.Response) => Item.Search(req, res));
 
 app.listen(1996, function() {
    Mongo.Connect();
--- src/Items.ts
+++ src/Items.ts
@@ -5,6 +5,7 @@ import { Collection, Cursor, Db, MongoClient, ObjectID } from "mongodb";
 import { Items } from '../typedef/types';
 
 export class Item {
+   // static collection: Collection<Items> = Mongo.client.db("Mordor").collection<Items>("items");
    static async FetchAll(request: e.Request, response: e.Response): Promise<void> {
       try {
          let ItemsCollection: Cursor<Items> = await Mongo.client.db("Mordor").collection<Items>("items").find({});
@@ -19,7 +20,6 @@ export class Item {
 
    static async FindById(request: e.Request, response: e.Response): Promise<void> {
       try {
-         console.log(request.params.id);
          await Mongo.client.db("Mordor").collection<Items>("items").findOne({ "_id": new ObjectID(request.params.id) }, function(err, result: Items) {
             if (err) {
                response.status(500).send(err).end();
@@ -29,6 +29,27 @@ export class Item {
          });
       } catch (e) {
          console.log(e);
+      }
+   }
+
+   static async Search(request: e.Request, response: e.Response): Promise<void> {
+      try {
+         if (request.body === undefined) {
+            response.status(500).end();
+         }
+
+         console.log(request.body);
+         await Mongo.client.db("Mordor").collection<Items>("items").findOne({ "itemName": request.body.key }, function(err, item) {
+            if (err) {
+               console.log(err);
+               response.status(500).send(err);
+            } else {
+               console.log(item);
+               response.status(200).send(item);
+            }
+         })
+      } catch (e) {
+         //
       }
    }
 }
