9499847d4c104359732d0a2bdd5b0208a3c85a2b
Abhiram13 <abhiram131869@gmail.com>
2020-11-17 19:47:09 +0530

    changes in search functionality

--- helpers/helper.ts
+++ helpers/helper.ts
@@ -101,23 +101,27 @@ export function Database<T, O>(collection: string, options: O): IOperations {
 
       Search: async function(request, response) {
          try {
-            await database.find({}).toArray().then(function(items) {
-               let array: T[] = [];
-               for (var i: number = 0; i < items.length; i++) {
-                  let item: { [key: string]: any; } = items[i];
-                  if (item[options as unknown as string].substring(0, request.body.string.length).toUpperCase() === request.body.string.toUpperCase()) {
-                     array.push(items[i]);
+            if (options) {
+               await database.find({}).toArray().then(function(items) {
+                  let array: T[] = [];
+                  for (var i: number = 0; i < items.length; i++) {
+                     let item: { [key: string]: any; } = items[i];
+                     if (item[options as unknown as string].substring(0, request.body.string.length).toUpperCase() === request.body.string.toUpperCase()) {
+                        array.push(items[i]);
+                     }
                   }
-               }
 
-               if (array.length > 0) {
-                  response.status(200).send(array);
-               } else {
-                  response.status(404).end();
-               }
-            });
+                  if (array.length > 0) {
+                     response.status(200).send(array);
+                  } else {
+                     response.status(404).end();
+                  }
+               });
+            } else {
+               response.status(200).send(await database.find({}).toArray());
+            }       
          } catch (e) {
-
+            response.status(500).send(e).end();
          }
       }
    };
--- routes/users.ts
+++ routes/users.ts
@@ -6,4 +6,6 @@ const userRouter = express.Router();
 
 userRouter.get('/findall', (req, res) => Database<User, string>("users", "").FindAll(req, res));
 
+userRouter.post('/search', (req, res) => Database<User, string>("users", "firstname").Search(req, res));
+
 export default userRouter;
